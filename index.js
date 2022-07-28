402057779ff6824ab8ef3aaf89febf4ae5a2fa46
Casres <christian@casillas.tech>
2022-07-28 02:53:45 -0400

    Added a view budget function

--- index.js
+++ index.js
@@ -757,3 +757,22 @@ function deleteDepartment() {
         })
     });
 }
+
+function viewBudget() {
+    console.log(chalk.yellow.bold(`====================================================================================`));
+    console.log(`                              ` + chalk.blue.bold(`Budget By Department:`));
+    console.log(chalk.yellow.bold(`====================================================================================`));
+
+    const sql =     `SELECT department_id AS id, 
+    department.department_name AS department,
+              SUM(salary) AS budget
+              FROM  role 
+              INNER JOIN department ON role.department_id = department.id GROUP BY role.department_id`;
+    connection.query(sql, (error, response) => {
+        if (error) throw error;
+        console.table(response);
+        console.log(chalk.yellow.bold(`====================================================================================`));
+
+        mainMenu();
+    });
+}
